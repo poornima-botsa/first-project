6dcfdc6a5091f716ceed29b126a0fad74dbb3397
poornima <botsavenkatasai@gmail.com>
2025-05-09 08:45:07 +0530

    added skills file

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import About from "../components/About";
-// import Skills from "../components/Skills";
+import Skills from "../components/Skills";
 // import Strengths from "../components/Strengths";
 import Projects from "../components/Projects";
 import Contact from "../components/Contact";
@@ -14,8 +14,8 @@ export default function Home() {
       <Navbar />
       <Hero />
       <About />
-      {/*<Skills />
-      <Strengths /> */}
+      <Skills />
+      {/*<Strengths /> */}
       <Projects />
       <Contact />
       {/* <DownloadResume /> */}
